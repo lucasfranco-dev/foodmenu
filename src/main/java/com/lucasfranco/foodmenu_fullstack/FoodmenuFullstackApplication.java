2805456faf31397e670456c05d6018f8d99be559
luczinn <lucfpscs@gmail.com>
2024-07-18 17:50:55 -0300

    feat: start configs

---
+++ src/main/java/com/lucasfranco/foodmenu_fullstack/FoodmenuFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.lucasfranco.foodmenu_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmenuFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmenuFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasfranco/foodmenu_fullstack/FoodmenuFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasfranco.foodmenu_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmenuFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
